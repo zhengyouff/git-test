bc4ce6e396a1655d9d0e53b4da26d12092a199c3
zhengyoufang <1073178934@qq.com>
2022-04-02 16:15:21 +0800

    push commit

--- src/main/java/com/zbj/git/GitTest.java
+++ src/main/java/com/zbj/git/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("master test");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
